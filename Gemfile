72036aa9a5b6f74392acb67c9d0cd4c3d16e259a
Ambre Margerit <ambremgt@gmail.com>
2020-02-12 20:01:36 +0100

    model + view + controller + routes

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'jbuilder', '~> 2.5'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'simple_form'
+gem 'font-awesome-sass', '~> 5.6.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -45,6 +47,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'autoprefixer-rails'
 end
 
 group :test do
@@ -57,5 +60,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-gem 'rspec-rails', group: [ :test ]
+gem 'rspec-rails', '4.0.0.beta3', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "06 21 38 72 79", category: "french" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "06 21 38 72 70", category: "italian" }
+mcdo =  { name: "McDo", address: "Rue Oberkampf", phone_number: "06 21 38 62 80", category: "french" }
+gladine = { name: "Gladine", address: "Rue Brochant", phone_number: "06 21 3 72 11", category: "french" }
+pignon =  { name: "Pignon", address: "Rue Réaumur", phone_number: "06 12 38 72 01", category: "japanese" }
+
+[dishoom, pizza_east, mcdo, gladine, pignon].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
